3734294e0f625b977218043dd16055d0397b81ed
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-04 20:31:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+dodger.style.backgroundColor = "#FF69B4";
+//moves the DODGER to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
+//Move dodger to the right
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
